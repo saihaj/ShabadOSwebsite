52b7ff7e33376df541345aef61944c16277a282d
Bhajneet S.K <bhajneet@gmail.com>
2021-08-15 11:01:07 -0400

    feat: add section block component

---
+++ src/components/Section.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from 'react'
+import { createUseStyles } from 'react-jss'
+
+type SectionProps = {
+  children?: ReactNode,
+  background?: string,
+  color?: string,
+  padding?: string,
+}
+
+const useStyles = createUseStyles( () => ( {
+  section: ( props: SectionProps ) => ( {
+    background: props.background || 'linear-gradient(#FFFFFF, #FAF8F7)',
+    color: props.color || 'black',
+    padding: props.padding,
+  } ),
+} ) )
+
+const Section = ( { children, ...props }: SectionProps ) => {
+  const classes = useStyles( { ...props } )
+  return (
+    <div className={classes.section}>
+      {children}
+    </div>
+  )
+}
+
+export default Section
